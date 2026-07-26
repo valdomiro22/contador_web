d31110f219160cc6381ddcc21693b47b5a37bf1f
valdomiro santos <valdomiro.jose@outlook.com>
2026-07-26 15:52:21 -0300

    formulario adicionar contador

--- src/app/features/contador/pages/adicionar-contador-page/adicionar-contador-page.ts
+++ src/app/features/contador/pages/adicionar-contador-page/adicionar-contador-page.ts
@@ -1,13 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { ReactiveFormsModule, Validators, FormBuilder } from "@angular/forms";
+import { ContadorService } from "../../contador.service";
+import { validate } from "@angular/forms/signals";
 
 @Component({
     selector: 'app-adicionar-contador-page',
       imports: [
-
+        ReactiveFormsModule,
       ],
       templateUrl: './adicionar-contador-page.html',
       styleUrls: ['./adicionar-contador-page.scss'],
 })
 export class AdicionarContadorPage {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly contadorService = inject(ContadorService);
 
+  formulario = this.formBuilder.nonNullable.group({
+    nome: ['', [
+      Validators.required,
+      Validators.maxLength(100)
+    ]], 
+
+    valorAtual: [0, [
+      Validators.required
+    ]],
+
+    incremento: [1, [
+      Validators.required,
+      Validators.min(1)
+    ]],
+
+    decremento: [1, [
+      Validators.required,
+      Validators.min(1)
+    ]],
+
+    valorInicial: [0],
+    valorMinimo: [0],
+    valorMaximo: this.formBuilder.control<number | null>(null),
+    meta: this.formBuilder.control<number | null>(null),
+  })
+
+  adicionar() {
+    console.log('adicionar contador')
+  }
 }
