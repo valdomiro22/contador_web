67c33b51cced8464440468c7c0bb2ced21f68ffa
valdomiro santos <valdomiro.jose@outlook.com>
2026-07-26 19:36:42 -0300

    adicionar mascara no display

--- src/app/features/contador/components/contador-display/contador-display.ts
+++ src/app/features/contador/components/contador-display/contador-display.ts
@@ -5,8 +5,12 @@ import { Contador } from "../../models/contador";
     selector: 'app-contador-display',
     imports: [],
     templateUrl: './contador-display.html',
-    styleUrls: ['./contador-display.scss']
+    styleUrls: ['./contador-display.scss'],
 })
 export class ContadorDisplay {
     readonly contador = input.required<Contador>()
+
+    formatarValor(valor: number): string {
+        return valor.toString().padStart(6, '0')
+    }
 }
